e4006c33606f5454876e06b3fc65189f241a2600
Patrick Bunda <1834214+paddybun@users.noreply.github.com>
2018-12-02 01:23:15 +0100

    added simple particle system

--- Laufdrueber/Assets/Scripts/bullet.cs
+++ Laufdrueber/Assets/Scripts/bullet.cs
@@ -1,13 +1,20 @@
 ﻿using UnityEngine;
 
 public class bullet : MonoBehaviour {
+
     private float ellapsedTime = 0f;
+
+    public float lifetime;
+    public ParticleSystem particleSystem;
 
 	void Update()
     {
         ellapsedTime += Time.deltaTime;
-        if (ellapsedTime >= 2f)
+        if (ellapsedTime >= lifetime)
         {
+            var ps = Instantiate(particleSystem);
+            ps.transform.position = transform.position;
+            ps.Play();
             gameObject.SetActive(false);
             ellapsedTime = 0;
         }
